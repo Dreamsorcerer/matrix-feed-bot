97817d11247f5dc194b6b9c3db3f812d288e9164
Isaac Beverly <imbev@protonmail.com>
2022-08-03 23:53:42 +0000

    Add config validation

--- matrix_rss_bridge/main.py
+++ matrix_rss_bridge/main.py
@@ -1,5 +1,25 @@
+import toml
+
+
+def validate_config(file_name):
+  config = toml.load(file_name)
+  try:
+    config['homeserver']
+    config['username']
+    config['password']
+    config['bridge']
+    for bridge in config['bridge']:
+      bridge['name']
+      bridge['feed_url']
+      bridge['room_id']
+  except KeyError as e:
+    raise ValueError(f"{e} not found in {file_name}")
+  return config
+
+
 def main():
-    print("hello world")
+  config = validate_config("config.toml")
+  print(config)
 
 if __name__ == '__main__':
     main()
